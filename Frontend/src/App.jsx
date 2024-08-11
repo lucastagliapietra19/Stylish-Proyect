1f30668c75b8670d863b9f8534327af233fa9a84
Lucas Tagliapietra <lucastagliapietra@hotmail.com>
2024-08-10 23:03:42 -0300

    landing page i

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,32 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//react router dom
+import {
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
+
+// Vistas (Views)
+
+import LadingPage from "./components/LandingPage/LandingPage";
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <routes>
+      {/* 1.-Ruta Principal - LadingPage */}
+      <Route
+          path="/"
+          element={
+            <>
+              <LadingPage />
+            </>
+          }
+        ></Route>
+    </routes>    
   )
 }
 
