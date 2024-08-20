4f2b4e9a110adcce9e4ee26825a07861689b8dac
Lucas Tagliapietra <lucastagliapietra@hotmail.com>
2024-08-19 23:23:15 -0300

    home

--- Frontend/src/components/Homepage/Homepage.jsx
+++ Frontend/src/components/Homepage/Homepage.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../css/homepage.css'; 
+
+const Homepage = () => {
+
+    return (
+        ""
+    );
+}
+export default Homepage;
