c7e4343fbe9a31a4dcebf051f49083a9287c1a0e
Lucas Tagliapietra <lucastagliapietra@hotmail.com>
2024-08-20 23:15:40 -0300

    head

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../css/header.css'
 
+const Header = () => {
+
+    return (
+        ""
+    );
+}
+
 export default Header;
